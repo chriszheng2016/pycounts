904e87daf7b32d12f50dfa7038c4cb96194e7d92
chriszheng2016 <chriszheng@vip.sina.com.cn>
2022-06-03 08:54:37 +0000

    feat: add example data and datasets module

--- src/pycounts/__init__.py
+++ src/pycounts/__init__.py
@@ -1,3 +1,8 @@
 # read version from installed package
+import imp
 from importlib.metadata import version
 __version__ = version("pycounts")
+
+# populate package namespace
+from pycounts.pycounts import count_words
+from pycounts.plotting import plot_words
